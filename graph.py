9c90a67bdc0623fd327887e5b961c1afdd277de8
Ibrahim Almakky <almakkyi@uni.coventry.ac.uk>
2021-02-08 11:47:14 +0000

    Graph Depth First Search

--- graph.py
+++ graph.py
@@ -3,8 +3,15 @@ class Graph:
     def __init__(self):
         self.map = {}
 
+
     def add_node(self, node):
         self.map[node] = []
+
+
+    def add_nodes(self, nodes):
+        for node in nodes:
+            self.add_node(node)
+
 
     def add_edge(self, node1, node2):
         try:
@@ -12,9 +19,40 @@ class Graph:
             self.map[node2].append(node1)
         except:
             print("Make sure both nodes already exist in the graph.")
-    
+
+
+    def add_edges(self, edges):
+        for edge in edges:
+            self.add_edge(edge[0], edge[1])
+
+
+    # Depth first search implementation
+    def depth_first_search(self, start, end):
+        visited = []
+        current_path = [start]
+        current_node = start
+        while current_node != end:
+            # Mark the current node as visited
+            visited.append(current_node)
+            # Get the nodes that the current node is connected to
+            connected_nodes = self.map[current_node]
+            # Check the first node the current node is connected but has not been visited
+            dead_end = True
+            for connected_node in connected_nodes:
+                if connected_node not in visited:
+                    current_node = connected_node
+                    dead_end = False
+                    break
+            if dead_end:
+                del current_path[-1]
+                current_node = current_path[-1]
+            else:
+                current_path.append(current_node)
+            print(current_path)
+                     
+
+
 my_graph = Graph()
-my_graph.add_node("E")
-my_graph.add_node("F")
-my_graph.add_edge("E", "F")
-print(my_graph.map)
+my_graph.add_nodes(["C","D","E","F","G","H","I"])
+my_graph.add_edges([("D", "C"),("D", "E"),("E", "F"),("F", "G"),("G", "H"),("H", "I")])
+my_graph.depth_first_search("D","I")
